177bbe9cac98fd8a36253ee2aa0d487a2d7d8469
ketan <ketan.thombare@gmail.com>
2024-06-18 15:27:21 +0530

    added all changes

--- src/main/java/com/demo/service/ServiceCities.java
+++ src/main/java/com/demo/service/ServiceCities.java
@@ -3,7 +3,7 @@ package com.demo.service;
 public class ServiceCities {
 
 	public String cities () {
-		return "mumbai,pune,banglore,hyderabad,demo-city";
+		return "mumbai,pune,banglore,hyderabad,demo-city,supe";
 	}
 
 	
